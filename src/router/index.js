92149226cd8e9795c637d23571ed24449d219526
joaovnpt <joaovitorguiotti8@gmail.com>
2024-12-20 00:24:27 -0300

    Feat: Criar página provisória de signin

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: () => import('../views/HomeView.vue'),
+      path: "/",
+      name: "Home",
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: '/movies',
-      name: 'Movies',
-      component: () => import('../views/MoviesView.vue')
+      path: "/movies",
+      name: "Movies",
+      component: () => import("../views/MoviesView.vue"),
     },
     {
-      path: '/tv',
-      name: 'TV',
-      component: () => import('../views/TvView.vue'),
+      path: "/tv",
+      name: "TV",
+      component: () => import("../views/TvView.vue"),
     },
     {
-      path: '/movie/:movieId',
-      name: 'MovieDetails',
-      component: () => import('../views/MovieDetailsView.vue'),
+      path: "/movie/:movieId",
+      name: "MovieDetails",
+      component: () => import("../views/MovieDetailsView.vue"),
       props: true,
     },
     {
-      path: '/tv/:tvShowId',
-      name: 'TvShowDetails',
-      component: () => import('../views/TvShowDetailsView.vue'),
+      path: "/tv/:tvShowId",
+      name: "TvShowDetails",
+      component: () => import("../views/TvShowDetailsView.vue"),
       props: true,
-    }
+    },
+    {
+      path: "/signin",
+      name: "SignIn",
+      component: () => import("../views/SignInView.vue")
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
@@ -40,6 +45,6 @@ const router = createRouter({
       return { top: 0 };
     }
   },
-})
+});
 
-export default router
+export default router;
---
+++ src/views/SignInView.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <p>No sign in page yet...</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p {
+    font-size: 1.5rem
+}
+</style>
