2dcf0aecf366ca34a8678772f43a8a438b447bfb
joaovnpt <joaovitorguiotti8@gmail.com>
2024-12-19 22:43:32 -0300

    Feat: Criar HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,91 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <h1>Which is on your vibe now?</h1>
+    <div class="options">
+      <button href="" ><router-link class="router" to="/movies">Movies</router-link></button>
+      <button href="" class="btn-1"><router-link class="router" to="/tv">Series</router-link></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 80vh;
+  gap: 3rem;
+}
+h1 {
+    margin: 0 auto;
+  font-size: 4rem;
+  width: 890px;
+  animation: digitando 3s steps(25), blink .4s infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 2px solid;
+}
+
+@keyframes digitando {
+    from {
+        width: 0;
+    }
+}
+
+@keyframes blink {
+    40%{
+        border-color: #ffffff2c;
+    }
+}
+
+.options {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+}
+
+.router {
+    text-decoration: none;
+}
+
+.btn-1 {
+    background-image: linear-gradient(50deg, #831dcc, #5B42F3 50%)
+}
+
+button {
+  align-items: center;
+  background-image: linear-gradient(144deg, #5B42F3 50%,#831dcc);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.158) 5px 5px 5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  display: flex;
+  font-size: 20px;
+  justify-content: center;
+  line-height: 1em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 19px 24px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+  transition: 200ms ease-in-out;
+}
+
+button:hover, .btn-1:hover {
+    transform: scale(1.04);
+}
+
+button:active,
+button:hover {
+  outline: 0;
+}
+</style>
